6768a9da5599b04d5390d5419c9fced5dc937c78
Miguel Yurivilca <miguel.yurivilca.16@gmail.com>
2020-06-11 10:36:36 -0500

    Landing page completed

---
+++ client/components/NavBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="sidebar" app>
+      <v-list>
+        <v-list-item
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            {{ item.title }}
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <span class="hidden-sm-and-up">
+        <v-app-bar-nav-icon @click="sidebar = !sidebar" />
+      </span>
+      <v-toolbar-title>
+        <nuxt-link to="/" tag="span" style="cursor: pointer;">
+          {{ appTitle }}
+        </nuxt-link>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn v-for="item in menuItems" :key="item.title" text :to="item.path">
+          <v-icon left dark>
+            {{ item.icon }}
+          </v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      appTitle: 'Educhay',
+      sidebar: false,
+      menuItems: [
+        { title: 'Home', path: '/', icon: 'mdi-home' },
+        { title: 'Registrarse', path: '/register', icon: 'mdi-face' },
+        { title: 'Iniciar sesion', path: '/login', icon: 'mdi-lock-open' }
+      ]
+    }
+  }
+}
+</script>
