aa69bed9b4be197d6711732a35bf41b360cb6a90
Flrotm <francesco.uccelli@utec.edu.pe>
2020-07-05 21:27:55 -0500

    bucket y amazonclient,cors??

---
+++ back/src/main/java/com/educhay/project/classes/AmazonClient.java
@@ -0,0 +1,101 @@
+package com.educhay.project.classes;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@Service
+public class AmazonClient {
+
+    private AmazonS3 s3client;
+
+    @Value("s3.us-east-1.amazonaws.com")
+    private String endpointUrl;
+    @Value("educhayvod-dev-input-gf3i00yl")
+    private String bucketName;
+    @Value("AKIAXRZ2O7TSTA2IULLG")
+    private String accessKey;
+    @Value("evDD3IW1SG45iH3BNbTBcoNry9hbT62cxGlOFDx7")
+    private String secretKey;
+
+    private Logger logger = LoggerFactory.getLogger(AmazonClient.class);
+
+    @PostConstruct
+    private void initializeAmazon() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        this.s3client = new AmazonS3Client(credentials);
+    }
+
+    public String uploadFile(MultipartFile multipartFile) throws IOException {
+
+        String fileUrl = "";
+        try {
+            File file = convertMultiPartToFile(multipartFile);
+            String fileName = generateFileName(multipartFile);
+            fileUrl = endpointUrl + "/" + bucketName + "/" + fileName;
+            uploadFileTos3bucket(fileName, file);
+            file.delete();
+        }
+        catch (AmazonServiceException ase) {
+            logger.info("Caught an AmazonServiceException from GET requests, rejected reasons:");
+            logger.info("Error Message:    " + ase.getMessage());
+            logger.info("HTTP Status Code: " + ase.getStatusCode());
+            logger.info("AWS Error Code:   " + ase.getErrorCode());
+            logger.info("Error Type:       " + ase.getErrorType());
+            logger.info("Request ID:       " + ase.getRequestId());
+
+
+        } catch (AmazonClientException ace) {
+            logger.info("Caught an AmazonClientException: ");
+            logger.info("Error Message: " + ace.getMessage());
+        } catch (IOException ioe) {
+            logger.info("IOE Error Message: " + ioe.getMessage());
+
+        }
+        return fileUrl;
+
+    }
+
+    public String deleteFileFromS3Bucket(String fileUrl) {
+        String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+        s3client.deleteObject(new DeleteObjectRequest(bucketName + "/", fileName));
+        return "Successfully deleted";
+    }
+
+    private void uploadFileTos3bucket(String fileName, File file) {
+        s3client.putObject(
+                new PutObjectRequest(bucketName, fileName, file));
+    }
+
+    private String generateFileName(MultipartFile multiPart) {
+        return new Date().getTime() + "-" + multiPart.getOriginalFilename().replace(" ", "_");
+    }
+
+    private File convertMultiPartToFile(MultipartFile file) throws IOException {
+        File convFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
+    }
+
+}
