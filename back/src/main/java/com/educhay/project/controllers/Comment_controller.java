1689643450ecd22f962f4d8a8815952bb2319a67
Nadnus <fernando.delosheros@utec.edu.pe>
2020-07-08 00:32:59 -0500

    cors

--- back/src/main/java/com/educhay/project/controllers/Comment_controller.java
+++ back/src/main/java/com/educhay/project/controllers/Comment_controller.java
@@ -15,13 +15,11 @@ import org.hibernate.criterion.Order;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-
+@CrossOrigin
+@RestController
 public class Comment_controller {
     @Autowired
     Video_repository video_repository;
